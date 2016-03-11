7c053754ae92bf21645144eea2342174bda419e2
Koala-t <johnsonc.88@gmail.com>
2016-03-11 15:28:26 -0800

    Make SVG canvas

--- client/watchout.js
+++ client/watchout.js
@@ -1,1 +1,21 @@
 // start slingin' some d3 here.
+var xyCreator = function(boardWidth, boardHeight) {
+  return [Math.random() * boardWidth, Math.random() * boardHeight];
+};
+
+var numEnemies = function (num) {
+  var results = [];
+  for (var i = 0; i < num; i++) {
+    results.push(xyCreator(100, 100));
+  }
+  return results;
+};
+
+d3.select('.board').selectAll("svg")
+  .data([1])
+  .enter()
+  .append("svg")
+  .style("width", "800px")
+  .style("height", "600px");
+
+
