fdf5eaa7e61867f9107729fae19966135b899684
Koala-t <johnsonc.88@gmail.com>
2016-03-11 20:16:46 -0800

    Finish scoreboard

--- client/watchout.js
+++ client/watchout.js
@@ -1,28 +1,28 @@
 // start slingin' some d3 here.
+var collisions = 0, highScore = 0, currScore = 0, numPointsPerSec = 10;
+var enemyCount = 5; // why missing one?
+var enemySpeed = 2000;
+
 var xyCreator = function(boardWidth, boardHeight) {
   return [Math.random() * boardWidth, Math.random() * boardHeight];
 };
 
-var numEnemies = function (num) {
+var enemyCreator = function (num) {
   var results = [];
-  for (var i = 0; i < num; i++) {
+  for (var i = 0; i < num; i++) {// can use <= to add the missing asteroid
     results.push(xyCreator(800, 600));
   }
   return results;
 };
 
-var enemyCount = 30;
-var enemySpeed = 1000;
+var changeEnemyPositions = function () {
+  d3.select('svg').selectAll('.asteroid')
+    .data(enemyCreator(enemyCount))
+    .transition().duration(enemySpeed)
+    .attr('cx', function (d) { return d[0]; }) 
+    .attr('cy', function (d) { return d[1]; });
+};
 
-// var click = function(){
-//   // Ignore the click event if it was suppressed
-//   // if (d3.event.defaultPrevented) return;
-//   // Extract the click location\
-//   debugger;    
-//   var point = d3.mouse(this);
-//   var p = {x: point[0], y: point[1] };
-// };
-//from a copy-paste; what does this accomplish?
 
 var dragmove = function(d) {
   var x = d3.event.x;
@@ -34,8 +34,30 @@ var dragmove = function(d) {
 var drag = d3.behavior.drag()
     .on('drag', dragmove);
 
+var gameOver = function() {
+  currScore = 0;
+  highScore = Math.max(currScore, highScore);
 
+  d3.select('.board')
+    .transition().duration(500)
+    .style('background-color', 'red')
+    .transition().duration(500)
+    .style('background-color', 'black');
+    //TO DO make the board flash red on collision
 
+  d3.select('.collisions').selectAll('span') 
+    .text('' + (collisions));
+};
+
+// var click = function(){
+//   // Ignore the click event if it was suppressed
+//   // if (d3.event.defaultPrevented) return;
+//   // Extract the click location\
+//   debugger;    
+//   var point = d3.mouse(this);
+//   var p = {x: point[0], y: point[1] };
+// };
+//from a copy-paste; what does this accomplish?
 
 d3.select('.board').selectAll('svg')
   .data([null])
@@ -56,29 +78,38 @@ d3.select('svg').selectAll('svg')
   .call(drag);
 
 d3.select('svg').selectAll('circle')
-  .data(numEnemies(enemyCount))
+  .data(enemyCreator(enemyCount))
   .enter()
   .append('circle')
   .classed('asteroid', true)
   .attr('r', '15')
   .attr('cx', function (d) { return d[0]; }) //REFACTOR IN ES6
   .attr('cy', function (d) { return d[1]; })
-  .style('fill', 'red');
+  .style('fill', 'red')
+  .on('mouseover', function() {
+    collisions++;
+    gameOver();
+  });
 
-var changeEnemyPositions = function () {
-  d3.select('svg').selectAll('.asteroid')
-    .data(numEnemies(enemyCount))
-    .transition().duration(enemySpeed)
-    .attr('cx', function (d) { return d[0]; }) 
-    .attr('cy', function (d) { return d[1]; });
-};
 
 
 setInterval(changeEnemyPositions, enemySpeed);
 
 
+var moreScore = function () {
+  currScore++;
+  d3.select('.current').selectAll('span') 
+    .text('' + currScore);
+
+  highScore = Math.max(highScore, currScore);
+  d3.select('.highscore').selectAll('span') 
+    .text('' + highScore);
+  
+
+};
 
 
+setInterval(moreScore, 1000 / numPointsPerSec);
 
 
 
